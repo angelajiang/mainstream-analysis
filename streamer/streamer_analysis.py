eb90b2d47662389aa0ad59bd1c07e93ae0706a30
Angela Jiang <to.ajiang@gmail.com>
2017-06-05 12:46:58 -0400

    Plot shared experiment with actual throughput values Also plot camera and transformer throughput values Remove bottleneck graph, since it's implied by the last processor

--- streamer/streamer_analysis.py
+++ streamer/streamer_analysis.py
@@ -18,9 +18,9 @@ def plot_simultaneous(csv_file):
     with open(csv_file) as f:
         for line in f:
             vals = line.split(',')
-            num_apps = int(vals[0])
+            num_NNs = int(vals[0])
             fps = float(vals[1])
-            xs.append(num_apps)
+            xs.append(num_NNs)
             ys.append(fps)
     width = 0.5
     plt.bar(xs, ys, width=width, color="lightcoral")
@@ -59,85 +59,79 @@ def plot_partial(csv_file):
 
 def plot_shared(csv_file):
     layers = []
-    num_apps = []
+    num_NNs = []
     data = {}
     with open(csv_file) as f:
         for line in f:
             vals = line.split(',')
             op_full = vals[0]
             layer = op_to_layer(op_full)
-            num_app = int(vals[1])
-            base = float(vals[2])
-            task = float(vals[3])
-            if num_app not in data.keys():
-                data[num_app] = {}
-            data[num_app][layer] = {}
-            data[num_app][layer]["base"] = base
-            data[num_app][layer]["task"] = task
+            num_NN = int(vals[1])
+            camera = float(vals[2])
+            transformer = float(vals[3])
+            base = float(vals[4])
+            task = float(vals[5])
+            if num_NN not in data.keys():
+                data[num_NN] = {}
+            data[num_NN][layer] = {}
+            data[num_NN][layer]["camera"] = camera
+            data[num_NN][layer]["transformer"] = transformer
+            data[num_NN][layer]["base"] = base
+            data[num_NN][layer]["task"] = task
             if layer not in layers:
                 layers.append(layer)
-            if num_app not in num_apps:
-                num_apps.append(num_app)
+            if num_NN not in num_NNs:
+                num_NNs.append(num_NN)
 
-    for num_app in num_apps:
-        base_fps = [data[num_app][layer]["base"] for layer in layers]
+    for num_NN in num_NNs:
+        base_fps = [data[num_NN][layer]["base"] for layer in layers]
         xs = range(len(layers))
-        plt.plot(xs, base_fps, label=str(num_app) + " apps")
+        plt.plot(xs, base_fps, label=str(num_NN) + " NNs")
     plt.xticks(xs, layers, rotation="vertical")
     plt.tick_params(axis='y', which='major', labelsize=28)
     plt.tick_params(axis='y', which='minor', labelsize=20)
     plt.ylabel("Base NN Throughput (FPS)", fontsize=28)
     plt.legend(loc=0, fontsize=15)
     plt.tight_layout()
-    plt.savefig("plots/shared/base.pdf")
+    plt.savefig("plots/shared-throughput/base.pdf")
     plt.clf()
 
-    for num_app in num_apps:
-        task_fps = [data[num_app][layer]["task"] for layer in layers]
+    for num_NN in num_NNs:
+        task_fps = [data[num_NN][layer]["task"] for layer in layers]
         xs = range(len(layers))
-        plt.plot(xs, task_fps, label=str(num_app) + " apps")
+        plt.plot(xs, task_fps, label=str(num_NN) + " NNs")
     plt.xticks(xs, layers, rotation="vertical")
     plt.tick_params(axis='y', which='major', labelsize=28)
     plt.tick_params(axis='y', which='minor', labelsize=20)
-    plt.yscale("log")
     plt.ylabel("Avg Task NN FPS", fontsize=28)
     plt.legend(loc=0, fontsize=15)
     plt.tight_layout()
-    plt.savefig("plots/shared/task.pdf")
+    plt.savefig("plots/shared-throughput/task.pdf")
     plt.clf()
 
-    for num_app in num_apps:
-        base_fps = [data[num_app][layer]["base"] for layer in layers]
-        task_fps = [data[num_app][layer]["task"] for layer in layers]
-        all_fps = zip(base_fps, task_fps)
-        bottleneck_fps = [min(pair_fps) for pair_fps in all_fps]
+    for num_NN in num_NNs:
         xs = range(len(layers))
-        plt.plot(xs, bottleneck_fps, label=str(num_app) + " apps")
-    plt.xticks(xs, layers, rotation="vertical")
-    plt.tick_params(axis='y', which='major', labelsize=28)
-    plt.tick_params(axis='y', which='minor', labelsize=20)
-    plt.ylabel("Bottleneck NN FPS", fontsize=28)
-    plt.legend(loc=0, fontsize=15)
-    plt.tight_layout()
-    plt.savefig("plots/shared/bottleneck.pdf")
-    plt.clf()
 
-    for num_app in num_apps:
-        base_fps = [data[num_app][layer]["base"] for layer in layers]
-        task_fps = [data[num_app][layer]["task"] for layer in layers]
-        xs = range(len(layers))
-        plt.plot(xs, base_fps, label="Base-"+str(num_app))
-        plt.plot(xs, task_fps, label="Task-"+str(num_app))
+        base_fps = [data[num_NN][layer]["base"] for layer in layers]
+        task_fps = [data[num_NN][layer]["task"] for layer in layers]
+        camera_fps = [data[num_NN][layer]["camera"] for layer in layers]
+        transformer_fps = [data[num_NN][layer]["transformer"] for layer in layers]
+        plt.plot(xs, base_fps, label="Base-"+str(num_NN))
+        plt.plot(xs, task_fps, label="Task-"+str(num_NN))
+        plt.plot(xs, camera_fps, label="Camera")
+        plt.plot(xs, transformer_fps, label="Transfomer")
+
+        # Format plot
         plt.xticks(xs, layers, rotation="vertical")
         plt.tick_params(axis='y', which='major', labelsize=28)
         plt.tick_params(axis='y', which='minor', labelsize=20)
         plt.ylabel("Throughput (FPS)", fontsize=28)
         plt.legend(loc=0, fontsize=15)
-        plt.title(str(num_app)+" split NN", fontsize=30)
-        plt.yscale("log")
-        plt.ylim(0, 1500)
+        plt.title(str(num_NN)+" split NN", fontsize=30)
+        plt.ylim(0, 32)
         plt.tight_layout()
-        plt.savefig("plots/shared/"+str(num_app)+"-NN.pdf")
+        plt.savefig("plots/shared-throughput/"+str(num_NN)+"-NN.pdf")
+        plt.clf()
 
 if __name__ == "__main__":
     cmd = sys.argv[1]
