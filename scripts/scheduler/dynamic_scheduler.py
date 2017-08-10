4d92cbf13f3ba631475902335bcc97f51846e6e0
Angela Jiang <to.ajiang@gmail.com>
2017-08-10 07:56:09 -0400

    Add static scheduler line to scheduler

--- scripts/scheduler/dynamic_scheduler.py
+++ scripts/scheduler/dynamic_scheduler.py
@@ -19,15 +19,15 @@ def plot(csv_file):
             vals = line.split(',')
             num_apps = int(vals[0])
             threshold = float(vals[1])
-            #err = math.floor(float(vals[3]) * 1000) / 1000
-            err = float(vals[2]) * num_apps
-            stdev = float(vals[4])
-            num_frozen_list = [float(v) for v in vals[4:]]
-            if threshold not in data.keys():
-                data[threshold] = {"xs":[], "ys":[]}
+            if threshold <= 9:
+                err = float(vals[2])
+                stdev = float(vals[4])
+                num_frozen_list = [float(v) for v in vals[4:]]
+                if threshold not in data.keys():
+                    data[threshold] = {"xs":[], "ys":[]}
 
-            data[threshold]["xs"].append(num_apps)
-            data[threshold]["ys"].append(err)
+                data[threshold]["xs"].append(num_apps)
+                data[threshold]["ys"].append(err)
 
     labels = []
 
@@ -40,6 +40,10 @@ def plot(csv_file):
         plt.plot(Xs, Ys, color=cycol(), lw=2)
         labels.append(str(int(threshold)) + " fps")
 
+    Ys = [.174] * len(Xs)
+    plt.plot(Xs, Ys, color="lightcoral", lw=2)
+    labels.append("Static scheduler")
+
     plt.legend(labels, loc=0)
 
     plt.tick_params(axis='y', which='major', labelsize=28)
@@ -47,10 +51,10 @@ def plot(csv_file):
     plt.tick_params(axis='x', which='major', labelsize=28)
     plt.tick_params(axis='x', which='minor', labelsize=20)
 
-    plt.xlabel("Number of apps", fontsize=20)
-    plt.ylabel("Sum rel acc loss", fontsize=20)
+    plt.xlabel("Number of apps", fontsize=25)
+    plt.ylabel("Avg Relative Top-1 Acc Loss", fontsize=25)
     plt.tight_layout()
-    plt.savefig("plots/scheduler/dynamic-scheduler.pdf")
+    plt.savefig("plots/scheduler/dynamic-scheduler-uniform.pdf")
     plt.clf()
 
 if __name__ == "__main__":
