a2f0a6f1bcbd6e7584e9610bbb7b6114165af3b1
Angela Jiang <to.ajiang@gmail.com>
2017-08-19 17:34:01 -0400

    Add preprocess util lib

---
+++ scripts/util/preprocess.py
@@ -0,0 +1,27 @@
+
+def op_to_layer(op_full):
+    tensor_name = (op_full.split(":"))[0]
+    layer = tensor_name.split("/")[0]
+    return layer
+
+def get_layers(csv_file, layers_index):
+    layers = []
+    with open(csv_file) as f:
+        for line in f:
+            vals = line.split(',')
+            op_full = vals[layers_index]
+            layer = op_to_layer(op_full)
+            if layer not in layers:
+                layers.append(layer)
+    return layers
+
+def get_num_NNs(csv_file):
+    num_NNs = []
+    with open(csv_file) as f:
+        for line in f:
+            vals = line.split(',')
+            num_NN = int(vals[1])
+            if num_NN not in num_NNs:
+                num_NNs.append(num_NN)
+    return num_NNs
+
