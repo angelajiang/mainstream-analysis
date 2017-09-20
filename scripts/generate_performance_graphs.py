d2f8bc270bdc92e35315e2d2ecedc11b03613c1d
Angela Jiang <to.ajiang@gmail.com>
2017-09-20 10:33:44 -0400

    Plot accuracy vs throughput in generate performance

--- scripts/generate_performance_graphs.py
+++ scripts/generate_performance_graphs.py
@@ -7,6 +7,8 @@ import dynamic_scheduler
 import scheduler
 sys.path.append("scripts/goodness")
 import accuracy_vs_layer
+sys.path.append("scripts/accuracy_vs_performance")
+import accuracy_tradeoffs
 
 import seaborn as sns
 
@@ -243,4 +245,26 @@ if __name__ == "__main__":
     scheduler.plot(ms_files, max_files, min_files, f_files, titles, plot_dir)
     scheduler.plot(ms_files, max_files, min_files, f_files_annotated, titles, plot_dir, True)
 
+    # Accuracy vs throughput
+
+    arch1 = "iv3"
+    arch2 = "r50"
+    arch3 = "mnets"
+
+    latency_file1 = "output/streamer/latency/inception/basic/latency-processors-iv3.csv"
+    latency_file2 = "output/streamer/latency/resnet/basic/latency-processors-r50.csv"
+    latency_file3 = "output/streamer/latency/mobilenets/basic/latency.csv"
+
+    accuracy_file1 = "output/mainstream/accuracy/flowers/inception/flowers-40-0.0001-dropout"
+    accuracy_file2 = "output/mainstream/accuracy/flowers/resnet/flowers-40-0.0001-chokepoints"
+    accuracy_file3 = "output/mainstream/accuracy/flowers/mobilenets/flowers-40-0.0001"
+
+    arches = [arch1, arch2, arch3]
+    latency_files = [latency_file1, latency_file2, latency_file3]
+    accuracy_files = [accuracy_file1, accuracy_file2, accuracy_file3]
+    labels = ["InceptionV3", "ResNet50", "MobileNets-224"]
+
+    plot_dir = "plots/tradeoffs/flowers"
+
+    accuracy_tradeoffs.plot_accuracy_vs_fps(arches, latency_files, accuracy_files, labels, plot_dir)
 
