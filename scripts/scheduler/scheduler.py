4b95af9466a2a97039626ceacd8baba549a1e40b
Angela Jiang <to.ajiang@gmail.com>
2017-10-10 11:42:22 -0400

    Plot fnr, fpr and f1

--- scripts/scheduler/scheduler.py
+++ scripts/scheduler/scheduler.py
@@ -5,7 +5,7 @@ import math
 import matplotlib
 import numpy as np
 from itertools import cycle
-from scipy.stats import linregress
+from scipy.stats import linregress, hmean
 
 sys.path.append("scripts/util")
 import plot_util
@@ -60,6 +60,59 @@ def get_fnr_data(csv_file, version):
         as1.append(round(np.average(acc_losses[x]), 2))
         as2.append(round(np.average(fpses[x]), 2))
     return xs, ys, errs, as1, as2
+
+def get_fpr_data(csv_file):
+    # Assumes Version 1
+    # Version 1: num_apps,fnr,fpr,acc_loss,fps_list...,frozen_list...
+    metrics = {}
+    xs = []
+    ys = []
+    errs = []
+    with open(csv_file) as f:
+        for line in f:
+            vals = line.split(',')
+            num_apps = int(vals[0])
+
+            if num_apps not in metrics.keys():
+                xs.append(num_apps)
+                metrics[num_apps] = []
+
+            fnr = float(vals[1])
+            fpr = float(vals[2])
+            metrics[num_apps].append(fpr)
+
+    for x in xs:
+        ys.append(np.average(metrics[x]))
+        errs.append(np.std(metrics[x]))
+    return xs, ys, errs
+
+
+def get_f1_data(csv_file):
+    # Assumes Version 1
+    # Version 1: num_apps,fnr,fpr,acc_loss,fps_list...,frozen_list...
+    metrics = {}
+    xs = []
+    ys = []
+    errs = []
+    with open(csv_file) as f:
+        for line in f:
+            vals = line.split(',')
+            num_apps = int(vals[0])
+
+            if num_apps not in metrics.keys():
+                xs.append(num_apps)
+                metrics[num_apps] = []
+
+            fnr = float(vals[1])
+            fpr = float(vals[2])
+            f1 = hmean([1 - float(fnr), 1 - float(fpr)])
+            metrics[num_apps].append(f1)
+
+    for x in xs:
+        ys.append(np.average(metrics[x]))
+        errs.append(np.std(metrics[x]))
+    return xs, ys, errs
+
 
 def plot_fnr(ms_files, max_files, min_files, plot_files, titles, plot_dir, annotated=False, version=0):
     for i in range(2):
@@ -125,7 +178,85 @@ def plot_fnr(ms_files, max_files, min_files, plot_files, titles, plot_dir, annot
             plt.tight_layout()
             plt.gca().xaxis.grid(True)
             plt.gca().yaxis.grid(True)
-            plt.savefig(plot_dir + "/" + plot_file + ".pdf")
+            plt.savefig(plot_dir + "/" + plot_file + "-fnr.pdf")
+            plt.clf()
+
+def plot_fpr(ms_files, max_files, min_files, plot_files, titles, plot_dir):
+    for i in range(2):
+        for ms_file, max_file, min_file, plot_file, title \
+                in zip(ms_files, max_files, min_files, plot_files, titles):
+            xs1, ys1, errs1 = get_fpr_data(ms_file)
+            xs2, ys2, errs2 = get_fpr_data(max_file)
+            xs3, ys3, errs3 = get_fpr_data(min_file)
+
+            plt.errorbar(xs3, ys3, yerr=errs3, lw=4, markersize=8,
+                         marker=plot_util.NO_SHARING['marker'],
+                         color=plot_util.NO_SHARING['color'],
+                         label=plot_util.NO_SHARING['label'])
+            plt.errorbar(xs2, ys2, yerr=errs2, lw=4, markersize=8,
+                         marker=plot_util.MAX_SHARING['marker'],
+                         color=plot_util.MAX_SHARING['color'],
+                         label=plot_util.MAX_SHARING['label'])
+            plt.errorbar(xs1, ys1, yerr=errs1, lw=4, markersize=8,
+                         marker=plot_util.MAINSTREAM['marker'],
+                         color=plot_util.MAINSTREAM['color'],
+                         label=plot_util.MAINSTREAM['label'])
+
+            plt.legend(loc=0, fontsize=15)
+
+            plt.tick_params(axis='y', which='major', labelsize=28)
+            plt.tick_params(axis='y', which='minor', labelsize=20)
+            plt.tick_params(axis='x', which='major', labelsize=28)
+            plt.tick_params(axis='x', which='minor', labelsize=20)
+
+            #plt.title(title, fontsize=35)
+            plt.xlabel("Number of concurrent apps", fontsize=30)
+            plt.xlim(2, max(xs1))
+            plt.ylim(0, 1)
+            plt.ylabel("False Positive Rate", fontsize=30)
+            plt.tight_layout()
+            plt.gca().xaxis.grid(True)
+            plt.gca().yaxis.grid(True)
+            plt.savefig(plot_dir + "/" + plot_file + "-fpr.pdf")
+            plt.clf()
+
+def plot_f1(ms_files, max_files, min_files, plot_files, titles, plot_dir):
+    for i in range(2):
+        for ms_file, max_file, min_file, plot_file, title \
+                in zip(ms_files, max_files, min_files, plot_files, titles):
+            xs1, ys1, errs1 = get_f1_data(ms_file)
+            xs2, ys2, errs2 = get_f1_data(max_file)
+            xs3, ys3, errs3 = get_f1_data(min_file)
+
+            plt.errorbar(xs3, ys3, yerr=errs3, lw=4, markersize=8,
+                         marker=plot_util.NO_SHARING['marker'],
+                         color=plot_util.NO_SHARING['color'],
+                         label=plot_util.NO_SHARING['label'])
+            plt.errorbar(xs2, ys2, yerr=errs2, lw=4, markersize=8,
+                         marker=plot_util.MAX_SHARING['marker'],
+                         color=plot_util.MAX_SHARING['color'],
+                         label=plot_util.MAX_SHARING['label'])
+            plt.errorbar(xs1, ys1, yerr=errs1, lw=4, markersize=8,
+                         marker=plot_util.MAINSTREAM['marker'],
+                         color=plot_util.MAINSTREAM['color'],
+                         label=plot_util.MAINSTREAM['label'])
+
+            plt.legend(loc=0, fontsize=15)
+
+            plt.tick_params(axis='y', which='major', labelsize=28)
+            plt.tick_params(axis='y', which='minor', labelsize=20)
+            plt.tick_params(axis='x', which='major', labelsize=28)
+            plt.tick_params(axis='x', which='minor', labelsize=20)
+
+            #plt.title(title, fontsize=35)
+            plt.xlabel("Number of concurrent apps", fontsize=30)
+            plt.xlim(2, max(xs1))
+            plt.ylim(0, 1)
+            plt.ylabel("F1 Score", fontsize=30)
+            plt.tight_layout()
+            plt.gca().xaxis.grid(True)
+            plt.gca().yaxis.grid(True)
+            plt.savefig(plot_dir + "/" + plot_file + "-f1.pdf")
             plt.clf()
 
 if __name__ == "__main__":
@@ -147,3 +278,20 @@ if __name__ == "__main__":
     plot_fnr(ms_files, max_files, min_files, f_files, titles, plot_dir)
     plot_fnr(ms_files, max_files, min_files, f_files_annotated, titles, plot_dir, True)
 
+    ms1 = "output/streamer/scheduler/v1/scheduler-v1-500-c0.1664-mainstream"
+    max1 = "output/streamer/scheduler/v1/scheduler-v1-500-c0.1664-maxsharing"
+    min1 = "output/streamer/scheduler/v1/scheduler-v1-500-c0.1664-nosharing"
+    f1 ="scheduler-500-c0.1664-ll2"
+    t1 = ""
+    plot_dir = "plots/scheduler/v1/"
+
+    ms_files = [ms1]
+    max_files = [max1]
+    min_files = [min1]
+    f_files = [f1]
+    f_files_annotated = [f + "-annotated" for f in f_files]
+    titles = [t1]
+
+    plot_fnr(ms_files, max_files, min_files, f_files, titles, plot_dir)
+    plot_fpr(ms_files, max_files, min_files, f_files, titles, plot_dir)
+    plot_f1(ms_files, max_files, min_files, f_files, titles, plot_dir)
