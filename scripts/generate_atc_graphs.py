04bc3a2ec581df2ec78ec16647fd5a8217099837
Angela Jiang <to.ajiang@gmail.com>
2018-02-04 02:38:48 -0500

    Plot emph graphs

--- scripts/generate_atc_graphs.py
+++ scripts/generate_atc_graphs.py
@@ -20,9 +20,9 @@ min_files = [min1]
 f_files = [f1]
 f_files_annotated = [f + "-annotated" for f in f_files]
 titles = [t1]
-pedestrian_annotations = [5, 28, 2, 15, 8, 20]
+pedestrian_annotations = [5, 28, 2, 15, 1, 20]
 
-scheduler.plot_f1(ms_files, max_files, min_files, f_files, titles, plot_dir, annotations = pedestrian_annotations)
+#scheduler.plot_f1(ms_files, max_files, min_files, f_files, titles, plot_dir, annotations = pedestrian_annotations)
 scheduler_dual.plot_f1_dual(ms_files, max_files, min_files, f_files, titles, plot_dir)
 scheduler_dual.plot_recall_dual(ms_files, max_files, min_files, f_files, titles, plot_dir)
 scheduler_dual.plot_precision_dual(ms_files, max_files, min_files, f_files, titles, plot_dir)
@@ -43,9 +43,9 @@ scheduler_dual.plot_f1_dual(ms_files, max_files, min_files, f_files, titles, plo
 scheduler_dual.plot_recall_dual(ms_files, max_files, min_files, f_files, titles, plot_dir)
 scheduler_dual.plot_precision_dual(ms_files, max_files, min_files, f_files, titles, plot_dir)
 
-ms1 =  "output/streamer/scheduler/atc/f1/f1-4hybrid-mainstream-simulator"
-max1 = "output/streamer/scheduler/atc/f1/f1-4hybrid-maxsharing"
-min1 = "output/streamer/scheduler/atc/f1/f1-4hybrid-nosharing"
+ms1 =  "output/streamer/scheduler/atc/f1/f1-4hybrid-corr-emph-mainstream-simulator"
+max1 = "output/streamer/scheduler/atc/f1/f1-4hybrid-corr-emph-maxsharing"
+min1 = "output/streamer/scheduler/atc/f1/f1-4hybrid-corr-emph-nosharing"
 f1 ="f1-4hybrid"
 
 ms_files = [ms1]
@@ -53,7 +53,7 @@ max_files = [max1]
 min_files = [min1]
 f_files = [f1]
 titles = [t1]
-hybrid4_annotations = [1, 6, 1, 5, 3, 6]
+hybrid4_annotations = [2, 6, 1, 5, 0, 6]
 
 scheduler.plot_f1(ms_files, max_files, min_files, f_files, titles, plot_dir, annotations = hybrid4_annotations)
 scheduler.plot_f1(ms_files, max_files, min_files, f_files, titles, plot_dir)
@@ -76,9 +76,12 @@ min_files = [min1]
 f_files = [f1]
 titles = [t1]
 
-ms1 =  "output/streamer/scheduler/atc/f1/f1-4hybrid-mainstream-simulator"
-max1 = "output/streamer/scheduler/atc/f1/f1-4hybrid-maxsharing"
-min1 = "output/streamer/scheduler/atc/f1/f1-4hybrid-nosharing"
+scheduler.plot_f1(ms_files, max_files, min_files, f_files, titles, plot_dir)
+scheduler.plot_precision(ms_files, max_files, min_files, f_files, titles, plot_dir)
+
+ms1 =  "output/streamer/scheduler/atc/f1/f1-4hybrid-corr-emph-mainstream-simulator"
+max1 = "output/streamer/scheduler/atc/f1/f1-4hybrid-corr-emph-maxsharing"
+min1 = "output/streamer/scheduler/atc/f1/f1-4hybrid-corr-emph-nosharing"
 f1 ="f1-4hybrid-corr-emph"
 
 ms_files = [ms1]
@@ -88,6 +91,7 @@ f_files = [f1]
 titles = [t1]
 
 scheduler.plot_f1(ms_files, max_files, min_files, f_files, titles, plot_dir)
+scheduler.plot_precision(ms_files, max_files, min_files, f_files, titles, plot_dir)
 
 ms1 =  "output/streamer/scheduler/atc/f1/f1-4hybrid-corr1-mainstream-simulator"
 max1 = "output/streamer/scheduler/atc/f1/f1-4hybrid-corr1-maxsharing"
@@ -101,5 +105,6 @@ f_files = [f1]
 titles = [t1]
 
 scheduler.plot_f1(ms_files, max_files, min_files, f_files, titles, plot_dir)
+scheduler.plot_precision(ms_files, max_files, min_files, f_files, titles, plot_dir)
 
 
