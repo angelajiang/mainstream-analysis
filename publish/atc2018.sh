5fc2e6c16817ec78968f2e0711b35d9c023db798
Daniel Wong <wonglkd@gmail.com>
2018-02-05 16:19:17 -0500

    Added publish script for atc2018

---
+++ publish/atc2018.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Fig 5: Effect of sample-rate on recall
+# python scripts/goodness/false_neg_by_stride.py
+cp plots/frame-rate/frame-rate-afn-dependences-with-correlation.pdf ../mainstream-paper/atc2018/figures/plots
