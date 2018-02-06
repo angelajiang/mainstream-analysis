83d475c690bb8984ab9d8ec0a5e0a873dd83cfcb
Daniel Wong <wonglkd@gmail.com>
2018-02-05 20:56:36 -0500

    Amend atc2018.sh

--- publish/atc2018.sh
+++ publish/atc2018.sh
@@ -8,7 +8,7 @@ cp plots/frame-rate/frame-rate-afn-dependences-with-correlation.pdf $PLOT_DIR
 
 # Fig 6: F1 for no of concurrent apps
 # python scripts/generate_atc_graphs.py
-cp plots/scheduler/atc/maximize-f1/f1-4hybrid-f1-annotated.pdf $PLOT_DIR
+cp plots/scheduler/atc/maximize-f1/f1-4hybrid-f1-annotated.pdf $PLOT_DIR/scheduler
 
 # Fig 10: deploy-time-series
 # python scripts/deploy/visualize.py
