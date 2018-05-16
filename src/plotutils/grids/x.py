b44321ecaad001ba74b8890a16166b458a5cb66c
Angela Jiang <to.ajiang@gmail.com>
2018-05-15 20:39:21 -0400

    Fix x and y labels

--- src/plotutils/grids/x.py
+++ src/plotutils/grids/x.py
@@ -8,7 +8,7 @@ def num_apps(xs, ax=None):
     xs = set(x for xss in xs for x in xss)
     ax.set_xlim(max(min(xs), 2), max(xs))
     ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-    ax.set_xlabel("No of apps")
+    ax.set_xlabel("Number of concurrent apps")
 
 
 def budget(xs, ax=None):
